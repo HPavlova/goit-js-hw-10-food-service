659a17eb3e22c5d55734c4c026538170d1dbf0c1
HPavlova <77845327+HPavlova@users.noreply.github.com>
2021-07-18 16:33:26 +0300

    добавляет index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 import './sass/main.scss';
+
+const refs = {
+  switch: document.querySelector('#theme-switch-toggle'),
+  body: document.querySelector('body'),
+};
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+// localStorage.clear();
+
+window.addEventListener('load', onCurrentTheme);
+refs.switch.addEventListener('change', onChangeTheme);
+
+function onCurrentTheme() {
+  localStorage.getItem('ThemeDark', refs.switch.checked);
+  refs.switch.checked = JSON.parse(localStorage.getItem('ThemeDark', refs.switch.checked));
+  // console.log(refs.switch.checked);
+  onCurrentClass();
+}
+
+function onChangeTheme(event) {
+  event.preventDefault();
+  onCurrentClass();
+  localStorage.setItem('ThemeDark', refs.switch.checked);
+}
+
+function onCurrentClass() {
+  if (refs.switch.checked) {
+    refs.body.classList.remove(Theme.LIGHT);
+    // console.log(refs.switch.checked);
+    return refs.body.classList.add(Theme.DARK);
+  } else {
+    refs.body.classList.remove(Theme.DARK);
+    // console.log(refs.switch.checked);
+    return refs.body.classList.add(Theme.LIGHT);
+  }
+}
